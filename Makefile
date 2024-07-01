97dca5c8856f4cd366f1e9bb031917410b5aadd7
Alex Nelson <alexander.nelson@nist.gov>
2024-07-01 15:28:40 -0400

    Use CDO external-ontology shapes to check usage in instance data
    No effects were observed on Make-managed files.
    
    References:
    * https://github.com/casework/CASE-Examples/pull/146
    
    Signed-off-by: Alex Nelson <alexander.nelson@nist.gov>

--- Makefile
+++ Makefile
@@ -61,6 +61,11 @@ all: \
 	$(MAKE) \
 	  --directory dependencies/CASE-unstable-2.0.0 \
 	  .git_submodule_init.done.log
+	# CDO-Shapes-Time
+	test -r dependencies/CDO-Shapes-Time/README.md \
+	  || git submodule update --init dependencies/CDO-Shapes-Time
+	test -r dependencies/CDO-Shapes-Time/README.md \
+	  || (echo "ERROR:Makefile:CDO-Shapes-Time submodule README.md file not found, even though CDO-Shapes-Time submodule initialized." >&2 ; exit 2)
 	# Retrieve rdf-toolkit.jar.
 	$(MAKE) \
 	  --directory dependencies/CASE-develop \
---
+++ examples/illustrations/existence_intervals/postvisit.mk
@@ -0,0 +1,41 @@
+#!/usr/bin/make -f
+
+# Portions of this file contributed by NIST are governed by the
+# following statement:
+#
+# This software was developed at the National Institute of Standards
+# and Technology by employees of the Federal Government in the course
+# of their official duties. Pursuant to Title 17 Section 105 of the
+# United States Code, this software is not subject to copyright
+# protection within the United States. NIST assumes no responsibility
+# whatsoever for its use by other parties, and makes no guarantees,
+# expressed or implied, about its quality, reliability, or any other
+# characteristic.
+#
+# We would appreciate acknowledgement if the software is used.
+
+SHELL := /bin/bash
+
+top_srcdir := $(shell cd ../../.. ; pwd)
+
+time_ttl := $(top_srcdir)/dependencies/CDO-Shapes-Time/dependencies/time.ttl
+sh_time_ttl := $(top_srcdir)/dependencies/CDO-Shapes-Time/shapes/sh-time.ttl
+
+all:
+
+.cdo-shapes.done.log: \
+  $(sh_time_ttl) \
+  $(time_ttl) \
+  $(top_srcdir)/.venv.done.log \
+  existence_intervals.json
+	source $(top_srcdir)/venv/bin/activate \
+	  && case_validate \
+	    --ontology-graph $(sh_time_ttl) \
+	    --ontology-graph $(time_ttl) \
+	    existence_intervals.json
+	touch $@
+
+check: \
+  .cdo-shapes.done.log
+
+clean:
